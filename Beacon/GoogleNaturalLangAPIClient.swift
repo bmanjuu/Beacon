4b7cabb0d9bb9b72a729a3993fd5fc28f954c7cc
bmanjuu <bettyofung@gmail.com>
2016-11-16 13:38:58 -0500

    obtained contents of sample JSON file

--- Beacon/GoogleNaturalLangAPIClient.swift
+++ Beacon/GoogleNaturalLangAPIClient.swift
@@ -10,4 +10,29 @@ import Foundation
 
 struct GoogleNaturalLangAPIClient {
     
+    //1. obtain JSON chat file (collaborate with Lloyd on this)
+    //2. direct this JSON chat file for the 'document' part of the HTTP call to the sentiment analyzer API
+    //3. make the API call (NSURLSession)
+    //4. assess the results 
+    //5. filter out negative stuff / keep neutralish - positive posts
+    
+//    static let chatJSONData = try? String(contentsOfFile: "testingLangAPI.json")
+    //json file is dictionary
+    
+    
+    //options: nsjsonserialization, bundle path stuff
+    
+    static func printStuff() {
+        print("CALLED FUNCTION")
+        
+        let jsonFilePath = Bundle.main.path(forResource: "testingLangAPI", ofType: "json")
+        print("JSON FILE PATH: \(jsonFilePath)")
+        let url = URL(fileURLWithPath: jsonFilePath!)
+        let data = try! Data(contentsOf: url)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        
+        print("JSON CONTENT HERE: \(json)")
+        
+    }
+    
 }
