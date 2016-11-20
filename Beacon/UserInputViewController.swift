cb72d8420a4f677ca58f250e21c4e03402561a52
TiciaChance <laticia.chance@flatironschool.com>
2016-11-20 12:40:20 -0500

    first view completed

---
+++ Beacon/UserInputViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserInputViewController.swift
+//  Beacon
+//
+//  Created by Laticia Chance on 11/20/16.
+//  Copyright © 2016 Betty Fung. All rights reserved.
+//
+
+import UIKit
+
+class UserInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
