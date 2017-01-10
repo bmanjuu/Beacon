57706e70868e6cbc9e8e378521db349065294842
Lloyd1nSpace <lloyd1nspace@icloud.com>
2017-01-10 12:20:29 -0500

    Created file for SignInVC

---
+++ Beacon/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  Beacon
+//
+//  Created by Lloyd W. Sykes on 1/10/17.
+//  Copyright © 2017 Betty Fung. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
